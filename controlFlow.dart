cbfc72954898f17a937cbf57c0ae1423e51bce89
kadelite <mechatronics.samson@gmail.com>
2025-10-03 23:39:23 +0100

    Control: for and while loop

---
+++ controlFlow.dart
@@ -0,0 +1,13 @@
+void main() {
+  // for loop
+  // for (int i = 0; i <= 5; i++) {
+  //   print('Iteration $i');
+  // }
+
+  //while loop
+  int count = 0;
+  while (count < 5) {
+    print('Count is $count');
+    count++;
+  }
+}

eb467e97e57d00b39166abf51b5d9b2224b6a148
kadelite <mechatronics.samson@gmail.com>
2025-10-04 10:04:28 +0100

    functions: Arrow, closure, and higher

--- functions.dart
+++ functions.dart
@@ -1,8 +1,20 @@
 void main() {
   // greet("Alice Adeleke", 30);
   // greet(userName: "Alice Adeleke", age: 30);
-  greet();
-}
+  //greet();
+
+  // int sum = add(5, 10);
+  // print('The sum is $sum');
+
+  //print(add(5, 6));
+
+  //Anonymous function
+  //var list = ["Apple", "Banana", "Orange", "Grapes"];
+  //list.forEach((item) => print(item));
+
+// defining a function
+  applyFunction(
+      5, (x) => x *= x); // Passing an anonymous function as an argument
 
 // void greet() {
 //   print('Hello, World! This is my first Dart function.');
@@ -19,6 +31,19 @@ void main() {
 // }
 
 //  optional positional parameters
-void greet([String? userName = "Adekunle", int? age = 20]) {
-  print('Hello, my name is $userName, I am $age years old.');
+// void greet([String? userName = "Adekunle", int? age = 20]) {
+//   print('Hello, my name is $userName, I am $age years old.');
+// }
+
+// returning values from functions
+// int add(int a, int b) {
+//   return a + b;
+// }
+
+// shorthand syntax for functions that return a single expression arrow function
+//int add(int a, int b) => a + b;
+}
+
+void applyFunction(int x, Function f) {
+  print(f(x)); // Calling the passed function with the argument x
 }

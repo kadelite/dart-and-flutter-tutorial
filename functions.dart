68c223f693a07b4f7d8054981d647890a0895d7f
kadelite <mechatronics.samson@gmail.com>
2025-10-04 08:40:23 +0100

    functions:Positional Parameters

--- functions.dart
+++ functions.dart
@@ -1,7 +1,12 @@
 void main() {
-  greet();
+  greet("Alice Adeleke", 30);
 }
 
-void greet() {
-  print('Hello, World! This is my first Dart function.');
+// void greet() {
+//   print('Hello, World! This is my first Dart function.');
+// }
+
+// positional parameters
+void greet(String userName, int age) {
+  print('Hello, my name is $userName and I am $age years old.');
 }

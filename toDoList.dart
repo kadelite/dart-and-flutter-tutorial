fb87fef438bea1df27460e5b5c20c766970d0d25
kadelite <mechatronics.samson@gmail.com>
2025-10-04 10:29:48 +0100

    toDoList App: menu function created

--- toDoList.dart
+++ toDoList.dart
@@ -17,4 +17,52 @@ void addTask(List<String> tasks) {
   stdout.write("Enter task description: ");
   String task = stdin.readLineSync()!;
   tasks.add(task);
+  print("Task added: $task");
+}
+
+void viewTasks(List<String> tasks) {
+  if (tasks.isEmpty) {
+    print("No tasks available.");
+  } else {
+    print("Your Tasks:");
+    for (int i = 0; i < tasks.length; i++) {
+      print("${i + 1}. ${tasks[i]}");
+    }
+  }
+}
+
+void removeTask(List<String> tasks) {
+  if (tasks.isEmpty) {
+    print("No tasks available.");
+    return;
+  }
+
+  viewTasks(tasks);
+  stdout.write("Enter the task number to remove: ");
+  int taskNumber = int.parse(stdin.readLineSync()!);
+
+  if (taskNumber < 1 || taskNumber > tasks.length) {
+    print("Invalid task number.");
+  } else {
+    String removedTask = tasks.removeAt(taskNumber - 1);
+    print("Task removed: $removedTask");
+  }
+}
+
+void markTaskCompleted(List<String> tasks, Set<int> completedTasks) {
+  if (tasks.isEmpty) {
+    print("No tasks available.");
+    return;
+  }
+
+  viewTasks(tasks);
+  stdout.write("Enter the task number to mark as completed: ");
+  int taskNumber = int.parse(stdin.readLineSync()!);
+
+  if (taskNumber < 1 || taskNumber > tasks.length) {
+    print("Invalid task number.");
+  } else {
+    completedTasks.add(taskNumber - 1);
+    print("Task marked as completed: ${tasks[taskNumber - 1]}");
+  }
 }

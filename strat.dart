3b00ce9839738d0d01fb80d7d2171500fab8db59
kadelite <mechatronics.samson@gmail.com>
2025-10-11 02:54:26 +0100

    Function: Closure, Higer order

--- strat.dart
+++ strat.dart
@@ -1,7 +1,38 @@
 void main() {
-  greet("Adekunle", 30);
+  //greet("Adekunle", 30);
+  // int result = add(10, 5);
+  // print("The sum is: $result");
+
+  // var list = ["Apple", "Banana", "Mango", "Orange"];
+  // //list.forEach((item) => print("I love $item") );
+  // list.forEach((item) {
+  //   print("I love $item");
+  // });
+  //var multiplyBy2 = createMultiplier(2);
+  //print(multiplyBy2(5)); // Output: 10
+  applyFunction(
+    5,
+    (x) => x * x,
+  ); // Passing an anonymous function as an argument
 }
 
-void greet([String? name, int? age]) {
-  print("Hello My name is $name and I am $age years old");
+// void greet([String? name, int? age]) {
+//   print("Hello My name is $name and I am $age years old");
+// }
+
+// int add(int a, int b) {
+//   return a + b;
+// }
+
+//int add(int a, int b) => a + b; // arrow function syntax
+
+//Anonymous function as parameter
+//Closure function
+// Function createMultiplier(num factor) {
+//   return (num i) => factor * i;
+// }
+// higher-order function
+
+void applyFunction(int x, Function f) {
+  print(f(x)); // Calling the passed function with the argument x
 }

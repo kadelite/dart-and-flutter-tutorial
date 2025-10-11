9e908ce57ba5ee24b67dfe6bf10fff3868d45c82
kadelite <mechatronics.samson@gmail.com>
2025-10-11 04:41:36 +0100

    OOP2: Mixin

---
+++ mixin.dart
@@ -0,0 +1,30 @@
+// Mixin
+
+void main() {
+  Fish myFish = Fish();
+  myFish.swim();
+  myFish.display();
+}
+
+mixin Swimmer {
+  void swim() {
+    print("...Swimming");
+  }
+}
+mixin Flyer {
+  void fly() {
+    print("...Flying");
+  }
+}
+
+class Duck with Swimmer, Flyer {
+  void display() {
+    print("I am a duck");
+  }
+}
+
+class Fish with Swimmer {
+  void display() {
+    print("I am a fish");
+  }
+}

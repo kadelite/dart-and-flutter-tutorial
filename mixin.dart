3501f1cc17f647700acfa99cb7b5325154cbdc47
kadelite <mechatronics.samson@gmail.com>
2025-10-31 22:48:15 +0100

    OOP: mixin

--- mixin.dart
+++ mixin.dart
@@ -1,13 +1,16 @@
 // Mixin in dart: allow to use method from one class in another class without inheritance.. sharing behaviours between classes
 // Use the 'with' keyword to apply mixins to a class
 
+/*
+Mixins are a way of reusing the code in multiple classes. Mixins are declared using the keyword mixin followed by the mixin name. Three keywords are used while working with mixins: mixin, with, and on. It is possible to use multiple mixins in a class. Cannot be instantiated, no constructors.
+*/
 void main() {
-  Fish myFish = Fish();
-  myFish.swim();
+  Fish myFish = Fish(); // object
+  myFish.swim(); // calling mixin method
   myFish.display();
 }
 
-mixin Swimmer {
+mixin Swimmer { // mixin
   void swim() {
     print("...Swimming");
   }

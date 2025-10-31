0b0d16c05a4ff6bd9a65c6a1e25aabd4d41eb742
kadelite <mechatronics.samson@gmail.com>
2025-10-31 22:24:18 +0100

    OOP: getter and setter in dart

--- inheritance.dart
+++ inheritance.dart
@@ -1,18 +1,22 @@
-// Inherintance in Dart?
+// Inheritance in Dart?
+/*
+Inheritance is a sharing of behaviour between two classes. It allows you to define a class that extends the functionality of another class. The extend keyword is used for inheriting from parent class.
+*/
 void main() {
-  Car myCar = Car("Red", "Tesla"); // object
+  Car myCar = Car("Red", "Tesla", ""); // object
   print("My car is a ${myCar.color} ${myCar.model}"); // Accessing properties
 }
 
 class Vehicle {
-  String color; // property
+  String color;
+  String name; // property
 
-  Vehicle(this.color); // constructor
+  Vehicle(this.color, this.name); // constructor
 }
 
 class Car extends Vehicle {
   String model; // property
 
-  Car(String color, this.model)
-    : super(color); // calling the parent class constructor
+  Car(String color, String name, this.model)
+    : super(color, name); // calling the parent class constructor
 }
